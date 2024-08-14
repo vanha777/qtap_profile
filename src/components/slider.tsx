dc15be9af46975e1f02397ce3e01ad9483110c18
vanha777 <vanha777@icloud.com>
2024-08-15 08:02:39 +1000

    x

--- src/components/slider.tsx
+++ src/components/slider.tsx
@@ -5,39 +5,7 @@ import Link from 'next/link';
 import { useEffect, useState } from "react";
 import { stringify } from 'querystring';
 import Card from './card';
-
-// Define the types for social and media objects
-type Social = {
-    link: string;
-    platforms: string;
-    icons: string;
-};
-
-type Media = {
-    media: string;
-    type: string;
-    info: string;
-};
-
-// Define the type for the user object
-type User = {
-    photo: string;
-    name: string;
-    title: string;
-    bio: string;
-    social: Social[];
-    media: Media[];
-};
-type Theme = {
-    background: string;
-    primary: string;
-    secondary: string;
-    inactiveColor?: string;
-    textColor?: string; // Optional property for text color
-    borderColor?: string;
-    buttonBackground: string
-    buttonText?: String
-};
+import { Theme,Media,User } from '../../themeConfig';
 
 // Define the props for the SliderItem component
 interface SliderItemProps {
